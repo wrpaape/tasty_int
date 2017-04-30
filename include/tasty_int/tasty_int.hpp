fefa15f9cc26cd5a013ea15ad7dd4d174c436c7c
Reid Paape <wrpaape@gmail.com>
2017-04-29 22:19:15 -0500

    std::conditional for sizing digit|acc_type

--- include/tasty_int/tasty_int.hpp
+++ include/tasty_int/tasty_int.hpp
@@ -3,11 +3,11 @@
 
 // EXTERNAL DEPENDENCIES
 // =============================================================================
-#include <vector>   // std::vector
-#include <string>   // std::string
-#include <iostream> // std::[i|o]stream
-#include <cstdint>  // UINT_MAX, uintmax_t
-#include <climits>  // U[LONG|INT|SHRT]_MAX
+#include <vector>      // std::vector
+#include <string>      // std::string
+#include <iostream>    // std::[i|o]stream
+#include <cstdint>     // std::uintmax_t
+#include <type_traits> // std::conditional
 
 
 // Interface
@@ -23,6 +23,9 @@ class TastyInt
     friend TastyInt &operator/(const TastyInt &lhs, const TastyInt &rhs);
     friend TastyInt &operator<<(const TastyInt &lhs, const TastyInt &rhs);
     friend TastyInt &operator>>(const TastyInt &lhs, const TastyInt &rhs);
+    // bitwise
+    friend TastyInt &operator&(const TastyInt &lhs, const TastyInt &rhs);
+    friend TastyInt &operator&(const TastyInt &lhs, const TastyInt &rhs);
     // comparison
     friend int compare(const TastyInt &lhs, const TastyInt &rhs);
     // IO
@@ -33,17 +36,34 @@ class TastyInt
 // -----------------------------------------------------------------------------
 public:
     // constructors
-    TastyInt();
-    TastyInt(const TastyInt &other) = default;
-    TastyInt &operator=(const TastyInt &other) = default;
-    TastyInt(const TastyInt &&other) = default;
+    TastyInt();                                // default
+    TastyInt(const TastyInt &other);           // copy
+    TastyInt(const TastyInt &&other) noexcept; // move
+    // destructor
+    ~TastyInt() noexcept;
+    // assignment
+    TastyInt &operator=(const TastyInt &other);           // copy
+    TastyInt &operator=(const TastyInt &&other) noexcept; // move
+    // swap
+    void swap(TastyInt &other) noexcept;
     // arithmetic
     TastyInt &operator+=(const TastyInt &other);
     TastyInt &operator-=(const TastyInt &other);
     TastyInt &operator*=(const TastyInt &other);
     TastyInt &operator/=(const TastyInt &other);
+    TastyInt &operator%=(const TastyInt &other);
+    TastyInt operator+() const;
+    TastyInt operator-() const;
+    // bitwise
     TastyInt &operator<<=(const TastyInt &other);
     TastyInt &operator>>=(const TastyInt &other);
+    TastyInt &operator&=(const TastyInt &other);
+    TastyInt &operator|=(const TastyInt &other);
+    TastyInt &operator^=(const TastyInt &other);
+    TastyInt operator~() const;
+    // conditional
+    explicit operator bool() const;
+    explicit operator!() const;
     // comparison
     bool operator==(const TastyInt &other) const;
     bool operator!=(const TastyInt &other) const;
@@ -51,29 +71,43 @@ public:
     bool operator>=(const TastyInt &other) const;
     bool operator<(const TastyInt &other) const;
     bool operator>(const TastyInt &other) const;
+    // string operations
+    std::string to_string(const unsigned int base = 10) const;
 
 private:
     // types
     //
-    // digit_type: unsigned integer, atomic unit of arithmetic
-    // acc_type:   large enough to hold the result of
-    //             'digit_type_max X digit_type_max'
-    typedef uintmax_t acc_type;
-#if   (UINTMAX_MAX > ULONG_MAX)
-    typedef unsigned long  digit_type;
-#elif (UINTMAX_MAX > UINT_MAX)
-    typedef unsigned int   digit_type;
-#elif (UINTMAX_MAX > USHRT_MAX)
-    typedef unsigned short digit_type;
-#else
-    typedef unsigned char  digit_type; // all integers same size
-#endif // if (UINT_MAX == ULONG_MAX)
+    // digit_type
+    //      unsigned integer, atomic unit of arithmetic
+    // acc_type
+    //      large enough to hold the result of
+    //      digit_type_max * digit_type_max
+    //      also
+    //      sizeof(digit_type) * 2 >= sizeof(acc_type)
+    //      -> 2 digits guaranteed to hold 1 acc
+    typedef std::uintmax_t acc_type;
+    typedef std::conditional<sizeof(unsigned char)      * 2 >= sizeof(acc_type),
+                unsigned char,
+            std::conditional<sizeof(unsigned short)     * 2 >= sizeof(acc_type),
+                unsigned short,
+            std::conditional<sizeof(unsigned int)       * 2 >= sizeof(acc_type),
+                unsigned int,
+            std::conditional<sizeof(unsigned long)      * 2 >= sizeof(acc_type),
+                unsigned long,
+            std::conditional<sizeof(unsigned long long) * 2 >= sizeof(acc_type),
+                unsigned long,
+                // no integer type smaller than acc_type that can express
+                // an acc in 2 variables, digit_type_max must be truncated
+                // from its native max value
+                acc_type
+            >::type>::type>::type>::type digit_type;
 
     // constants
     static const acc_type acc_type_max;
-    static const acc_type digit_type_max; // floor(sqrt(acc_type_max))
+    static const acc_type digit_type_max; // <= floor(sqrt(acc_type_max))
 
     // instance data
-    std::vector<digit_type> digits;
-    bool is_negative;
+    int sign;                       // -1, 0, 1
+    std::vector<digit_type> digits; // ordered least -> most significant
+                                    // (digits.front() -> digits.back())
 }
