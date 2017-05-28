b77ade43968b27c020d882d30e0c5bcf6a9c8c39
Reid Paape <wrpaape@gmail.com>
2017-05-28 14:47:33 -0500

    transfering corrected cmake testing setup from cmake_tutorial

--- cmake/add_catch_test.cmake
+++ cmake/add_catch_test.cmake
@@ -1,12 +1,11 @@
 if(PROJECT_CMAKE_ADD_CATCH_TEST_CMAKE_INCLUDED)
     return()
-endif(PROJECT_CMAKE_ADD_CATCH_TEST_CMAKE_INCLUDED)
+endif()
 set(PROJECT_CMAKE_ADD_CATCH_TEST_CMAKE_INCLUDED TRUE)
 # ==============================================================================
 # add a 'test_${NAME}' executable and CTest from sources compiled with the Catch
 # Test framework
 # ==============================================================================
-
 # External Dependencies
 # ------------------------------------------------------------------------------
 include(add_custom_test)
@@ -21,22 +20,31 @@ set(CATCH_TEST_HEADER ${CATCH_TEST_DIR}/catch.hpp)
 # Set Up Framework
 # ------------------------------------------------------------------------------
 # create directory in test/include
-file(MAKE_DIRECTORY ${CATCH_TEST_DIR})
+if(NOT EXISTS ${CATCH_TEST_DIR})
+    file(MAKE_DIRECTORY ${CATCH_TEST_DIR})
+endif()
 
 # download latest version of 'catch'
-file(
-    DOWNLOAD
-    https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp
-    ${CATCH_TEST_HEADER}
-)
+if(NOT EXISTS ${CATCH_TEST_HEADER})
+    message(
+        STATUS
+        "Downloading 'Catch' header to: ${CATCH_TEST_HEADER}"
+    )
+    file(
+        DOWNLOAD
+        https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp
+        ${CATCH_TEST_HEADER}
+        SHOW_PROGRESS
+    )
+endif()
 
 
 # External API
 # ------------------------------------------------------------------------------
 function(add_catch_test)
     add_custom_test(
-        FRAMEWOK_NAME     catch
-        FRAMEWORK_SOURCES ${CATCH_TEST_HEADER}
         ${ARGN}
+        FRAMEWORK_NAME    Catch
+        FRAMEWORK_SOURCES ${CATCH_TEST_HEADER}
     )
-endfunction(add_catch_test)
+endfunction()
---
+++ cmake/testing.cmake
@@ -0,0 +1,35 @@
+if(PROJECT_CMAKE_TESTING_CMAKE_INCLUDED)
+    return()
+endif()
+set(PROJECT_CMAKE_TESTING_CMAKE_INCLUDED TRUE)
+# ==============================================================================
+# enable unit testing
+# ==============================================================================
+# External Dependencies
+# ------------------------------------------------------------------------------
+include(CTest) # sets BUILD_TESTING
+
+
+# Exported Variables
+# ------------------------------------------------------------------------------
+# test directories
+set(PROJECT_TEST_INCLUDE_DIR   ${PROJECT_TEST_DIR}/include)
+set(PROJECT_TEST_SRC_DIR       ${PROJECT_TEST_DIR}/src)
+set(PROJECT_TEST_LIB_DIR       ${PROJECT_TEST_DIR}/lib)
+set(PROJECT_TEST_BIN_DIR       ${PROJECT_TEST_DIR}/bin)
+set(PROJECT_BUILD_TEST_SRC_DIR ${PROJECT_BUILD_DIR}/test/src)
+
+# build and run all custom tests with verbose output
+set(PROJECT_RUN_TESTS_TARGET run_tests)
+
+
+# Targets
+# ------------------------------------------------------------------------------
+# add 'make test' to run unit tests
+enable_testing()
+
+# add 'make run_tests' to build and run tests
+add_custom_target(
+    ${PROJECT_RUN_TESTS_TARGET}
+    COMMAND ${CMAKE_CTEST_COMMAND} --extra-verbose --output-on-failure
+)
