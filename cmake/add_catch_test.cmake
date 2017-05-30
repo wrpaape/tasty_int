8aac0e16dba05433978c132ba700e2fef6e01950
Reid Paape <wrpaape@gmail.com>
2017-05-30 18:18:03 -0500

    attempting to build

--- cmake/add_catch_test.cmake
+++ cmake/add_catch_test.cmake
@@ -1,6 +1,6 @@
 if(PROJECT_CMAKE_ADD_CATCH_TEST_CMAKE_INCLUDED)
     return()
-endif(PROJECT_CMAKE_ADD_CATCH_TEST_CMAKE_INCLUDED)
+endif()
 set(PROJECT_CMAKE_ADD_CATCH_TEST_CMAKE_INCLUDED TRUE)
 # ==============================================================================
 # add a 'test_${NAME}' executable and CTest from sources compiled with the Catch
@@ -21,21 +21,25 @@ set(CATCH_TEST_HEADER ${CATCH_TEST_DIR}/catch.hpp)
 # Set Up Framework
 # ------------------------------------------------------------------------------
 # create directory in test/include
-file(MAKE_DIRECTORY ${CATCH_TEST_DIR})
+if(NOT EXISTS ${CATCH_TEST_DIR})
+    file(MAKE_DIRECTORY ${CATCH_TEST_DIR})
+endif()
 
 # download latest version of 'catch'
-file(
-    DOWNLOAD
-    https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp
-    ${CATCH_TEST_HEADER}
-)
+if(NOT EXISTS ${CATCH_TEST_HEADER})
+    file(
+        DOWNLOAD
+        https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp
+        ${CATCH_TEST_HEADER}
+        SHOW_PROGRESS
+    )
+endif()
 
 
 # External API
 # ------------------------------------------------------------------------------
 function(add_catch_test)
     add_custom_test(
-        FRAMEWOK_NAME     catch
         FRAMEWORK_SOURCES ${CATCH_TEST_HEADER}
         ${ARGN}
     )
---
+++ src/tasty_int_demo/tasty_int_demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+
+int
+main()
+{
+    std::cout << "HELLO WORLD from " << __file__ << std::endl;
+    
+    return 0;
+}
