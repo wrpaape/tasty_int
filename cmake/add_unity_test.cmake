41bc19ed291f3e9f086e3fef1ab5cd0320d718c9
Reid Paape <wrpaape@gmail.com>
2017-06-09 20:23:16 -0500

    more on add_unity_test

--- cmake/add_unity_test.cmake
+++ cmake/add_unity_test.cmake
@@ -16,9 +16,11 @@ include(add_custom_test)
 
 # Exported Variables
 # ------------------------------------------------------------------------------
-set(UNITY_TEST_SRC_DIR           ${PROJECT_TEST_SRC_DIR}/Unity)
-set(UNITY_TEST_BUILD_DIR         ${PROJECT_BUILD_TEST_SRC_DIR}/Unity)
-set(PROJECT_CMAKE_UNITY_TEST_DIR ${CMAKE_CURRENT_LIST_DIR}/unity_test)
+set(UNITY_TEST_SRC_DIR    ${PROJECT_TEST_SRC_DIR}/Unity)
+set(UNITY_TEST_BUILD_DIR  ${PROJECT_BUILD_TEST_SRC_DIR}/Unity)
+set(UNITY_TEST_SCRIPT_DIR ${PROJECT_TEST_SCRIPT_DIR}/unity_test)
+set(UNITY_TEST_RUNNER_DIR ${PROJECT_TEST_RUNNER_DIR}/unity_test)
+set(UNITY_TEST_PATCH_DIR  ${CMAKE_CURRENT_LIST_DIR}/unity_test)
 set(
     UNITY_TEST_LIBRARIES
     unity_test
@@ -32,15 +34,15 @@ ExternalProject_Add(
     GIT_REPOSITORY      https://github.com/ThrowTheSwitch/Unity.git
     GIT_TAG             master
     PATCH_COMMAND       ${CMAKE_COMMAND} -E copy
-                        ${PROJECT_CMAKE_UNITY_TEST_DIR}/gitignore_patches
+                        ${UNITY_TEST_PATCH_DIR}/gitignore_patches
                         ${UNITY_TEST_SRC_DIR}/.gitignore
                         COMMAND
                         ${CMAKE_COMMAND} -E copy
-                        ${PROJECT_CMAKE_UNITY_TEST_DIR}/CMakeLists.txt
+                        ${UNITY_TEST_PATCH_DIR}/CMakeLists.txt
                         ${UNITY_TEST_SRC_DIR}/CMakeLists.txt
                         COMMAND
                         ${CMAKE_COMMAND} -E copy
-                        ${PROJECT_CMAKE_UNITY_TEST_DIR}/unity_config.h
+                        ${UNITY_TEST_PATCH_DIR}/unity_config.h
                         ${UNITY_TEST_SRC_DIR}/src/unity_config.h
     SOURCE_DIR          ${UNITY_TEST_SRC_DIR}
     BINARY_DIR          ${UNITY_TEST_BUILD_DIR}
@@ -51,11 +53,24 @@ ExternalProject_Add(
  
 # External API
 # ------------------------------------------------------------------------------
-function(add_unity_test)
+function(add_unity_test unity_test_source)
+    set(unity_test_runner_source
+        "${UNITY_TEST_RUNNER_DIR}/codegen/runner__${unity_test_source}")
+
     # TODO: test_runners
     add_custom_test(
         ${ARGN}
         FRAMEWORK_NAME      Unity
         FRAMEWORK_LIBRARIES ${UNITY_TEST_LIBRARIES}
+    )
+
+    add_custom_command(
+        OUTPUT            ${unity_test_runner_source}
+        COMMAND           ${RUBY_EXECUTABLE}
+                          ${UNITY_TEST_SCRIPT_DIR}/generate_test_runner.rb
+                          ${unity_test_source}
+                          ${unity_test_runner_source}
+        WORKING_DIRECTORY ${UNITY_TEST_SCRIPT_DIR}
+        COMMENT           "generate unity test runner for '${unity_test_source}'"
     )
 endfunction()
--- cmake/testing.cmake
+++ cmake/testing.cmake
@@ -13,11 +13,13 @@ include(CTest) # sets BUILD_TESTING
 # Exported Variables
 # ------------------------------------------------------------------------------
 # test directories
-set(PROJECT_TEST_INCLUDE_DIR   ${PROJECT_TEST_DIR}/include)
-set(PROJECT_TEST_SRC_DIR       ${PROJECT_TEST_DIR}/src)
-set(PROJECT_TEST_LIB_DIR       ${PROJECT_TEST_DIR}/lib)
-set(PROJECT_TEST_BIN_DIR       ${PROJECT_TEST_DIR}/bin)
-set(PROJECT_BUILD_TEST_SRC_DIR ${PROJECT_BUILD_DIR}/test/src)
+set(PROJECT_TEST_INCLUDE_DIR      ${PROJECT_TEST_DIR}/include)
+set(PROJECT_TEST_SRC_DIR          ${PROJECT_TEST_DIR}/src)
+set(PROJECT_TEST_RUNNER_DIR       ${PROJECT_TEST_SRC_DIR}/runner)
+set(PROJECT_TEST_LIB_DIR          ${PROJECT_TEST_DIR}/lib)
+set(PROJECT_TEST_BIN_DIR          ${PROJECT_TEST_DIR}/bin)
+set(PROJECT_TEST_SCRIPT_DIR       ${PROJECT_TEST_DIR}/script)
+set(PROJECT_BUILD_TEST_SRC_DIR    ${PROJECT_BUILD_DIR}/test/src)
 
 # build and run all custom tests with verbose output
 set(PROJECT_RUN_TESTS_TARGET run_tests)
