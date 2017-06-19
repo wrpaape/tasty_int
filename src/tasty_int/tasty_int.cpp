532e9bd7c793289bb99069e8963eb615c90675a6
Reid Paape <wrpaape@gmail.com>
2017-06-18 23:02:29 -0500

    fleshing out bytes_from_string

--- src/tasty_int/tasty_int.cpp
+++ src/tasty_int/tasty_int.cpp
@@ -16,6 +16,7 @@ unsigned int TastyInt::global_default_base = 10;
 // helper functions
 // -----------------------------------------------------------------------------
 namespace {
+
 [[ noreturn ]] inline void
 throw_from_string_no_valid_digits()
 {
@@ -41,13 +42,33 @@ throw_from_string_max_base_exceeded()
     );
 }
 
+// string of ASCII tokens    (1 -> length, most sig -> least sig) to
+// byte-wise array of values (1 -> length, least sig -> most sig)
 std::unique_ptr<unsigned char[]> 
-bytes_from_string(const unsigned char *string,
+bytes_from_string(const unsigned char *const CPP_RESTRICT_QUALIFIER string,
                   const std::size_t length,
                   const char *token_values)
 {
-    std::unique_ptr<unsigned char[]> bytes = std::make_unique<unsigned char[]>(length);
+    int value;
+
+    std::unique_ptr<unsigned char[]>
+    bytes = std::make_unique<unsigned char[]>(length);
+
+    unsigned char *CPP_RESTRICT_QUALIFIER byte        = bytes.get();
+    const unsigned char *CPP_RESTRICT_QUALIFIER token = &string[length];
+
+    do {
+        value = (int) token_values[*--token];
+
+        if (value < 0)
+            throw_from_string_invalid_digits();
+
+        *byte++ = (unsigned char) value;
+    } while (token != string);
+
+    return bytes;
 }
+
 } // namespace
 
 
@@ -82,21 +103,26 @@ TastyInt::TastyInt(const char *string,
     }
 
 
+    std::unique_ptr<unsigned char[]> bytes = ::bytes_from_string(
+        reinterpret_cast<const unsigned char *>(string),
+        length,
+        token_values
+    );
 
-    digits_from_string(reinterpret_cast<const unsigned char *>(string),
-                       length,
-                       base,
-                       token_values);
+    // digits_from_string(
+    //                    length,
+    //                    base,
+    //                    token_values);
 
     this->sign = ((digits.size() == 1) && (digits[0] == 0)) ? 0 : sign;
 }
 
-void
-TastyInt::digits_from_bytes(const unsigned char *begin,
-                            const std::size_t length,
-                            const unsigned int base)
-{
-    unsigned char *token = begin + length;
-}
+// void
+// TastyInt::digits_from_bytes(const unsigned char *begin,
+//                             const std::size_t length,
+//                             const unsigned int base)
+// {
+//     unsigned char *token = begin + length;
+// }
 
 
