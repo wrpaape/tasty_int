9e12e9675f04c65873abf67eea996e72cdc6f90e
Reid Paape <wrpaape@gmail.com>
2017-06-19 22:44:35 -0500

    adding include_directories/compile definitions to src/CMakeLists.txt

--- cmake/module/cpp_restrict_qualifier.cmake
+++ cmake/module/cpp_restrict_qualifier.cmake
@@ -6,23 +6,16 @@ set(PROJECT_CMAKE_CPP_RESTRICT_QUALIFIER_INCLUDED TRUE)
 # check C++ compiler's support for a 'restrict' extension keyword
 # set CPP_RESTRICT_QUALIFIER to that keyword or empty string if not supported
 # ==============================================================================
-# Exported Variables
-# ------------------------------------------------------------------------------
-set(CPP_RESTRICT_QUALIFIER_SRC_DIR
-    ${PROJECT_CMAKE_SRC_DIR}/cpp_restrict_qualifier)
-set(CPP_RESTRICT_QUALIFIER_BUILD_DIR
-    ${PROJECT_CMAKE_BUILD_DIR}/cpp_restrict_qualifier)
-
-
 # External API
 # ------------------------------------------------------------------------------
 function(set_cpp_restrict_qualifier)
-    set(supported_qualifier "")
-    foreach(qualifer "__restrict__" "__restrict" "restrict")
+    set(build_dir ${PROJECT_CMAKE_BUILD_DIR}/cpp_restrict_qualifier)
+    set(src_dir   ${PROJECT_CMAKE_SRC_DIR}/cpp_restrict_qualifier)
+    foreach(qualifer __restrict__ __restrict restrict)
         try_compile(
             qualifier_compiled
-            ${CPP_RESTRICT_QUALIFIER_BUILD_DIR}
-            ${CPP_RESTRICT_QUALIFIER_SRC_DIR}/test_cpp_restrict_qualifier.cpp
+            ${build_dir}
+            ${src_dir}/test_cpp_restrict_qualifier.cpp
             COMPILE_DEFINITIONS "-DCPP_RESTRICT_QUALIFIER=${qualifer}"
             OUTPUT_VARIABLE OOGA
         )
@@ -33,5 +26,7 @@ function(set_cpp_restrict_qualifier)
         endif()
     endforeach()
 
-    set(CPP_RESTRICT_QUALIFIER ${supported_qualifier} PARENT_SCOPE)
+    set(CPP_RESTRICT_QUALIFIER "${supported_qualifier}" PARENT_SCOPE)
 endfunction()
+
+set_cpp_restrict_qualifier()
